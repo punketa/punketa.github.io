0c0aabb21a77562caad251e403b20fc92153f071
dylan <diegodylanchamorro@gmail.com>
2026-06-01 15:31:23 +0200

    v1.2

--- punketa/js.js
+++ punketa/js.js
@@ -13,14 +13,11 @@ setInterval(AgeCounter, 50);
 
 async function loadSteamWidget() {
 
-    const widget =
-    document.getElementById("steam-widget");
+    const widget = document.getElementById("steam-widget");
 
-    const res =
-    await fetch("http://localhost:3000/api/steam");
+    const res = await fetch("http://localhost:3000/api/steam");
 
-    const data =
-    await res.json();
+    const data = await res.json();
 
     const states = {
 
@@ -34,96 +31,59 @@ async function loadSteamWidget() {
 
     };
 
-    const lastLogoff =
-    new Date(data.lastlogoff * 1000)
-    .toLocaleString();
+    const lastLogoff = new Date(data.lastlogoff * 1000).toLocaleString();
 
     widget.innerHTML = `
-
         <div class="steam-card">
-
-            <img
-                class="steam-avatar"
-                src="${data.avatar}"
-            >
-
+            <img class="steam-avatar" src="${data.avatar}">
             <div class="steam-user">
-
                 <h2>${data.username}</h2>
-
                 <div class="steam-realname">
                     ${data.realname}
                 </div>
-
                 <div class="steam-status">
-
                     ${data.game !== "Offline"
                         ? `Playing : ${data.game}`
                         : states[data.personastate]
                     }
-
                 </div>
-
                 <div class="steam-details">
-
                     <div>
                         <span>SteamID:</span>
                         ${data.steamid}
                     </div>
-
                     <div>
                         <span>GameID:</span>
                         ${data.gameid || "None"}
                     </div>
-
                     <div>
                         <span>Country:</span>
                         ${data.country}
                     </div>
-
                     <div>
                         <span>Last Logoff:</span>
                         ${lastLogoff}
                     </div>
-
                 </div>
-
             </div>
-
             <div class="steam-right">
-
                 <h3>
                     ${data.game !== "Offline"
                         ? "Playing:"
                         : "Favorite Game:"
                     }
                 </h3>
-
-                <img
-                    class="steam-banner"
-                    src="${data.banner}"
-                >
-
+                <img class="steam-banner" src="${data.banner}">
             </div>
-
             <div class="steam-bottom">
-
                 <div class="steam-level">
-
                     ${data.level}
-
                 </div>
-
                 <div class="steam-games">
-
                     ${data.totalGames} Games
-
                 </div>
-
             </div>
-
         </div>
-
     `;
 }
 
